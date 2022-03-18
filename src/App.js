4c5abd9f8b54e40352b90e75521ab9c08198eba1
Elmar <L-mar@inbox.ru>
2022-03-19 00:27:48 +0400

    Fix issue with fetching from API

--- src/App.js
+++ src/App.js
@@ -1,9 +1,49 @@
+import React from "react";
 import './App.css';
 
-const App = () => (
-  <div>
-    <h1>Hello there!</h1>
-  </div>
-);
-
+class App extends React.Component {
+   
+    // Constructor 
+    constructor(props) {
+        super(props);
+   
+        this.state = {
+            items: [],
+            DataisLoaded: false
+        };
+    }
+   
+    // ComponentDidMount is used to
+    // execute the code 
+    componentDidMount() {
+        fetch(
+"https://az-courses-api.herokuapp.com/courses/")
+            .then((res) => res.json())
+            .then((json) => {
+                this.setState({
+                    items: json,
+                    DataisLoaded: true
+                });
+            })
+    }
+    render() {
+        const { DataisLoaded, items } = this.state;
+        if (!DataisLoaded) return <div>
+            <h1> Pleses wait some time.... </h1> </div> ;
+   
+        return (
+        <div className = "App">
+            <h1> Fetch data from an api in react </h1>  {
+                items.map((item) => ( 
+                <ol key = { item.id } >
+                    Course_Name: { item.name }, 
+                    Description: { item.technologies }
+                    </ol>
+                ))
+            }
+        </div>
+    );
+}
+}
+   
 export default App;
