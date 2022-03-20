73f9d319efc04d131eae1072385a33182b8864af
Elmar <L-mar@inbox.ru>
2022-03-21 02:37:55 +0400

    Fetch data and create a header

---
+++ src/components/API/API.js
@@ -0,0 +1,59 @@
+import React, { Component } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './API.css';
+import Loading from '../Loading/Loading';
+import { Card, Button } from 'react-bootstrap';
+
+class API extends Component {
+    // Constructor 
+    constructor(props) {
+        super(props);
+   
+        this.state = {
+            items: [],
+            DataisLoaded: false
+        };
+    }
+    // ComponentDidMount is used to
+    // execute the code 
+    componentDidMount() {
+        fetch(
+"http://127.0.0.1:8000/courses/")
+            .then((res) => res.json())
+            .then((json) => {
+                this.setState({
+                    items: json,
+                    DataisLoaded: true
+                });
+            })
+    }
+    render() {
+        const { DataisLoaded, items } = this.state;
+        if (!DataisLoaded) return <div>
+            <Loading /></div> ;
+   
+        return (
+        <div className="courses">
+          <h1>Available courses</h1>
+          <div className="items-courses">
+          {
+            items.map((items) => (
+              <Card style={{ width: '18rem' }} className="item">
+                {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
+                <Card.Body>
+                  <Card.Title> { items.name } </Card.Title>
+                  <Card.Text>
+                    { items.technologies }
+                  </Card.Text>
+                  <Button variant="primary" className="cardButton">Go to course</Button>
+                </Card.Body>
+              </Card>
+            ))
+          }
+          </div>
+        </div>
+    );
+}
+}
+   
+export default API;
---
+++ src/components/Loading/Loading.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Loading.css';
+
+const Loading = () => (
+  <div className='loading'>
+    <div className='loader'>
+    </div>
+    <div>
+      <h2>Please, wait a while!</h2>
+    </div>
+  </div>
+);
+
+export default Loading;
