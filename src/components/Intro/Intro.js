b8162c684d9d4788cc755ce1b435cfd3652667e6
Elmar <L-mar@inbox.ru>
2022-03-23 16:39:58 +0400

    Fix linter issues

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -2,29 +2,32 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button, Container } from 'react-bootstrap';
 // import * as Icon from 'react-bootstrap-icons';
-import { FcCheckmark } from "react-icons/fc";
-import { FcSurvey } from "react-icons/fc";
-import { FcApproval } from "react-icons/fc";
+import { FcCheckmark, FcSurvey, FcApproval } from 'react-icons/fc';
 
 import './Intro.css';
 
 const Intro = () => (
-  <div className='intro'>
+  <div className="intro">
     <Container className="intro-content">
       <h1>How it works?</h1>
-      <ul className='list-how-it-works'>
-        <li><FcCheckmark />
-        Signup
-        </li>
-        <li><FcSurvey /> Check your English level</li>
-        <li><FcSurvey /> Get tested for technologies</li>
-        <li><FcApproval /> Get a least 70% of score</li>
+      <ul className="list-how-it-works">
+        <li>
+          <FcCheckmark />
+         Signup</li>
+        <li>
+          <FcSurvey /> Check your English level</li>
+        <li>
+          <FcSurvey />
+         Get tested for technologies</li>
+        <li>
+          <FcApproval />
+         Get a least 70% of score</li>
       </ul>
       <p>
         <Button variant="success" size="lg">Get started</Button>
       </p>
     </Container>
-    <Container className="intro-content" >
+    <Container className="intro-content">
       <img scr="./" alt="test-score" />
     </Container>
   </div>
