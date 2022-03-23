019af73fd8d632c8bb4ff3c9a5a1f4e0f7490570
Elmar <L-mar@inbox.ru>
2022-03-23 23:17:39 +0400

    Fix spaces

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -3,7 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button, Container } from 'react-bootstrap';
 // import * as Icon from 'react-bootstrap-icons';
 import { FcCheckmark, FcSurvey, FcApproval } from 'react-icons/fc';
-
+import intro from './intro.png';
 import './Intro.css';
 
 const Intro = () => (
@@ -13,22 +13,24 @@ const Intro = () => (
       <ul className="list-how-it-works">
         <li>
           <FcCheckmark />
-         Signup</li>
+          Signup
+        </li>
         <li>
-          <FcSurvey /> Check your English level</li>
+          <FcSurvey /> 
+          Check your English level
+        </li>
         <li>
           <FcSurvey />
-         Get tested for technologies</li>
+          Get tested for technologies
+        </li>
         <li>
           <FcApproval />
-         Get a least 70% of score</li>
+          Get a least 70% of score
+        </li>
       </ul>
       <p>
         <Button variant="success" size="lg">Get started</Button>
       </p>
-    </Container>
-    <Container className="intro-content">
-      <img scr="./" alt="test-score" />
     </Container>
   </div>
 );
