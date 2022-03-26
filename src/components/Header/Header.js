70ce4edc2469c901140f2e2bf646fa46d7410079
Elmar <L-mar@inbox.ru>
2022-03-27 01:28:29 +0400

    Fix linter issues

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,13 +2,16 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import './Header.css';
-import { ImLibrary } from "react-icons/im";
+import { ImLibrary } from 'react-icons/im';
 
 const Header = () => (
   <>
     <Navbar collapseOnSelect fixed="top" expand="sm" bg="dark" variant="dark">
       <Container>
-        <Navbar.Brand href="./"><ImLibrary className="logo-icon" />AZ-Courses</Navbar.Brand>
+        <Navbar.Brand href="./">
+          <ImLibrary className="logo-icon" />
+          AZ-Courses
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navsbar-nav">
           <Nav>
--- src/components/WhoUR/WhoUR.js
+++ src/components/WhoUR/WhoUR.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button, Container } from 'react-bootstrap';
-import { FcOk } from "react-icons/fc";
+import { FcOk } from 'react-icons/fc';
 import './Whour.css';
 
 const Whour = () => (
@@ -9,17 +9,40 @@ const Whour = () => (
     <Container className="whour-content">
       <h1>Who you are?</h1>
       <ul className="need-list">
-        <li><FcOk />A student or just graduated</li>
-        <li><FcOk />A beginer in a software engineering</li>
-        <li><FcOk />A developer with low expirience</li>
-        <li><FcOk />Just changing a sphere to IT</li>
+        <li>
+          <FcOk />
+          A student or just graduated</li>
+        <li>
+          <FcOk />
+          A beginer in a software engineering
+        </li>
+        <li>
+          <FcOk />
+          A developer with low expirience
+        </li>
+        <li>
+          <FcOk />
+          Just changing a sphere to IT
+        </li>
       </ul>
       <p className="add-needs">... additionally:</p>
       <ul className="need-list">
-        <li><FcOk />fast learner</li>
-        <li><FcOk />highly motivated</li>
-        <li><FcOk />responsible</li>
-        <li><FcOk />self-confident</li>
+        <li>
+          <FcOk />
+          fast learner
+        </li>
+        <li>
+          <FcOk />
+          highly motivated
+        </li>
+        <li>
+          <FcOk />
+          responsible
+        </li>
+        <li>
+          <FcOk />
+          self-confident
+        </li>
       </ul>
       <p>
         <Button bsStyle="primary" size="lg" className="need-signup">Signup now</Button>
